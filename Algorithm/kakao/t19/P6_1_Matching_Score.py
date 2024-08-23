4d423394015fd7a8c14cbe615bcfda0167779429
the9kim <the9rla@gmail.com>
2024-08-23 10:18:08 +0900

    kakao19 Q6-Matching-Score

--- Algorithm/kakao/t19/P6_1_Matching_Score.py
+++ Algorithm/kakao/t19/P6_1_Matching_Score.py
@@ -1,66 +1,49 @@
-from typing import List
 import collections
 import re
+from typing import List
+
+
 class P6_1_Matching_Score:
     '''
-    1. Extract information from HTML using Regex.
-    2. Calculate matching scores
+    1. Parse html pages
+    2. Calculate the optimal matching score
     '''
-    def solution(self, word:str, pages: List[str]):
-        url_map = collections.defaultdict(str)
-        link_map = collections.defaultdict(int)
-        word_map = collections.defaultdict(int)
-        linked_page_map = collections.defaultdict(list)
 
-        # 1.
-        for i, html in enumerate(pages):
-            html = html.lower()
-            word = word.lower()
+    def solution(self, word: str, pages: List[str]):
 
-            # 1-1 url
-            regex = "<meta property=\"og:url\" content=\"(.*?)\"/"
-            matcher = re.compile(regex)
-            matches = matcher.findall(html)
-            for match in matches:
-                url_map[i] = match
+        n = len(pages)
 
-            # 1-2 link
-            regex = "<a href=\"(.*?)\""
-            matcher = re.compile(regex)
-            matches = matcher.findall(html)
-            link_map[i] = len(matches)
-            for match in matches:
-                linked_page_map[match].append(i)
+        urls = []
+        basic_scores = []
+        links = collections.defaultdict(list)
+        matching_scores = []
 
-            # 1-3 word
-            regex = "(?<![a-z])" + re.escape(word) + "(?![a-z])"
-            matcher = re.compile(regex)
-            matches = matcher.findall(html)
-            word_map[i] = len(matches)
+        word = word.lower()
+        word_pattern = re.compile(r'(?<![a-zA-Z])' + re.escape(word) + r'(?![a-zA-Z])')
+        url_pattern = re.compile(r'<meta property="og:url" content="(.*?)"/>')
+        link_pattern = re.compile(r'<a href="(.*?)">')
 
-        # 2
-        max_score = -1
-        index = -1
         for i, page in enumerate(pages):
-            basic_score = word_map[i]
-            link_score = 0
-            linked_pages = linked_page_map[url_map[i]]
-            for idx in linked_pages:
-                link_score += word_map[idx] / link_map[idx]
-            total_score = basic_score + link_score
+            page = page.lower()
 
-            if total_score > max_score:
-                max_score = total_score
-                index = i
+            # URL
+            url = re.search(url_pattern, page)
+            urls.append(url.group(1) if url else '')
 
-        return index
+            # Word
+            basic_scores.append(len(re.findall(word_pattern, page)))
 
+            # Link
+            links[i].extend(re.findall(link_pattern, page))
 
+        for i in range(n):
+            score = basic_scores[i]
 
+            for j in range(n):
 
+                if i != j and urls[i] in links[j]:
+                    score += basic_scores[j] / len(links[j])
 
+            matching_scores.append(score)
 
-
-
-
-
+        return matching_scores.index(max(matching_scores))
