bc8031b7b3037b5543725527cb8069aa1b3d05bd
the9kim <the9rla@gmail.com>
2025-10-21 11:38:08 +0900

    refactor: kakao19 Q3-Candidate-Key

--- Algorithm/kakao/t19/P3_2_Candidate_Key.py
+++ Algorithm/kakao/t19/P3_2_Candidate_Key.py
@@ -4,28 +4,28 @@ from functools import cmp_to_key
 
 class P3_2_Candidate_Key:
 
-    def solution(relation):
+    def solution(self, relation):
 
         col_size = len(relation[0])
         candidate_keys = []
 
         for subset in range(1, (1 << col_size)):
-            if not _is_minimal(candidate_keys, subset):
+            if not self._is_minimal(candidate_keys, subset):
                 continue
 
-            if _is_unique(relation, subset):
+            if self._is_unique(relation, subset):
                 candidate_keys.append(subset)
 
         return len(candidate_keys)
 
-    def _is_minimal(candidate_keys, subset):
+    def _is_minimal(self, candidate_keys, subset):
         for key in candidate_keys:
             if (subset & key) == key:
                 return False
 
         return True
 
-    def _is_unique(relation, subset):
+    def _is_unique(self, relation, subset):
         seen = set()
 
         selected_cols = [i for i in range(len(relation[0])) if ((1 << i) & subset) != 0]
